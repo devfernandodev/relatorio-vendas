042dc0f9583bdb92845e58d91082e44856f83366
Luis Fernando <132279103+devfernandodev@users.noreply.github.com>
2024-10-23 23:58:54 -0300

    renomeando  relatorio_vendas.sql to procedure_relatorio_vendas.sql

--- procedure_relatorio_vendas.sql
+++ procedure_relatorio_vendas.sql
@@ -8,16 +8,16 @@ BEGIN
                v.quantidade
         FROM produtos p
         JOIN vendas v ON p.id_produto = v.id_produto
-        ORDER BY v.data_venda DESC  -- Ordenar por data da venda
+        ORDER BY v.data_venda DESC 
     ) LOOP
         DBMS_OUTPUT.PUT_LINE('Produto: ' || rec.produto || 
                              ', Categoria: ' || rec.categoria ||
-                             ', Preço: R$ ' || rec.preco || 
+                             ', PreÃ§o: R$ ' || rec.preco || 
                              ', Data da Venda: ' || TO_CHAR(rec.data_venda, 'DD/MM/YYYY') || 
                              ', Quantidade: ' || rec.quantidade);
     END LOOP;
 EXCEPTION
     WHEN OTHERS THEN
-        DBMS_OUTPUT.PUT_LINE('Erro ao gerar relatório: ' || SQLERRM);
+        DBMS_OUTPUT.PUT_LINE('Erro ao gerar relatÃ³rio: ' || SQLERRM);
 END;
 /
